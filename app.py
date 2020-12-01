dce2096f976dd7d662a881051bc66cbfdf60679a
klfoulk16 <>
2020-12-01 09:10:50 -0500

    Attempting to add user login feature

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-"""Set up all the essential flask peices and app routes. Connect database that
-contains the blog posts and information."""
+"""Set up all the essential flask peices and app routes. Connect database that contains the blog posts and information."""
 
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
+from flask_login import LoginManager, LoginForm
 import datetime
 
 # Configure app
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
+"""Set up the app config"""
 # Specify which environment we're using
 ENV = 'dev'
 
@@ -30,8 +20,6 @@ else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config.update(
 	#EMAIL SETTINGS
 	MAIL_SERVER='smtp.gmail.com',
@@ -40,11 +28,13 @@ app.config.update(
 	MAIL_USERNAME = 'foulkelly1@gmail.com',
 	MAIL_PASSWORD = '@RandomPassword',
     #MISC SETTINGS
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False,
+    # Ensure templates are auto-reloaded
+    TEMPLATES_AUTO_RELOAD = True
 	)
 
-mail = Mail(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 # if you need to redo the db setup, drop the table and then run python in terminal and then these commands:
     # >>> from app import db
@@ -66,6 +56,58 @@ class Posts(db.Model):
         self.body = body
         self.category = category
         self.date = datetime.date.today()
+
+class User(db.Model):
+    """An admin user capable of viewing reports.
+
+    :param str email: email address of user
+    :param str password: encrypted password for the user
+
+    """
+    __tablename__ = 'user'
+
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.secret_key = b'L@\xf1\xd6\xb0z\xb5\x8f\xc8Nj\xcat\xf9\xa7\x91'
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+
+    """
+    return User.query.get(user_id)
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 
 @app.route('/')
 def index():
@@ -93,12 +135,37 @@ def create():
 @app.route('/post/<id>', methods=['GET'])
 def edit(id):
     post = Posts.query.filter_by(id=id).first()
+    post.date = post.date.strftime('%B %d, %Y')
     return render_template('post.html', post=post)
 
 @app.route('/<category>', methods=['GET'])
 def post_layout(category):
     posts = Posts.query.filter_by(category=category)
+    for post in posts:
+        post.date = post.date.strftime('%B %d, %Y')
     return render_template('post_layout.html', posts=posts, category=category)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run()
