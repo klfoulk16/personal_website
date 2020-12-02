938aedf005669155ce39bbd4e5067a21802de54e
klfoulk16 <>
2020-12-02 09:39:28 -0500

    Switched environment to production for heroku deploy

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Specify which environment we're using
 ENV = 'dev'
 
-if ENV == 'dev':
+if ENV == 'prod':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = '***REMOVED***'
 else:
